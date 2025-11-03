f2fc709e8c9292fde4163b5bdf714248e73c5d95
KevinTss <kevin.tassi@gmail.com>
2025-11-03 20:37:45 +0100

    prefix stuff in prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/la-roche-triple" : undefined,
+  assetPrefix: isProd ? "/la-roche-triple/" : undefined,
   images: {
     unoptimized: true,
   },
