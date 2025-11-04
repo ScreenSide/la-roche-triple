9477b8cb5d88a091c9b50c55aa614b6c02a2a17a
KevinTss <KevinTss@users.noreply.github.com>
2025-11-04 17:42:47 +0000

    deploy: a6c003adc3863e7728a1e5f17e5c098ff3b57d9e

--- _next/static/YvFQM0gz2Nnc7Ums7kfOv/_buildManifest.js
+++ _next/static/YvFQM0gz2Nnc7Ums7kfOv/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/la-roche-triple//_next/:path+",
-        "destination": "/la-roche-triple/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
